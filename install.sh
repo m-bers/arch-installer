1f0f7ed465e4841838f316c174ead40ef1fa2cea
m-bers <jc4gavejc@gmail.com>
2021-03-04 20:27:58 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "Hello World"
+USERNAME="[username]"
+PASSWORD="[password]"
+DISK="/dev/sda"
+
+echo -e "g\nn\n\n\n+500M\nt\n1\nn\n\n\n\nt\n\n30\nw\n" | fdisk $DISK
+mkfs.vfat -F32 "${DISK}1"
+pvcreate --dataalignment 1m "${DISK}2"
+vgreate vg0 "${DISK}2"
+lvcreate -L 30GB vg0 -n root
+lvcreate -L 50GB vg0 -n home
+modprobe dm_mod
+vgchange -ay
+mkfs.ext4 /dev/vg0/root
+mount /dev/vg0/root /mnt
+mkdir /mnt/home
+mount /dev/vg0/home /mnt/home
+mkdir /mnt/etc
+genfstab -U -p /mnt >> /mnt/etc/fstab
+pacstrap /mnt base
